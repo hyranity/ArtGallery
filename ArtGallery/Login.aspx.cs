d9b14f3becaad0714b816856775b3cad2704a946
ollefrost <elcasaboxi@gmail.com>
2019-06-13 20:02:33 +0800

    test commit #5

--- ArtGallery/Login.aspx.cs
+++ ArtGallery/Login.aspx.cs
@@ -4,7 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls; // test commentsssss
+using System.Web.UI.WebControls;
 
 namespace ArtGalleryApp
 {
