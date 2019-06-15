feb8903ff7e76ec4ef347a4ba43563fff9047a73
Ryan <42234515+ollefrost@users.noreply.github.com>
2019-06-15 14:24:09 +0800

    Update ArtistDao.cs

--- ArtGallery/Daos/ArtistDao.cs
+++ ArtGallery/Daos/ArtistDao.cs
@@ -1,4 +1,4 @@
-public class ArtistDao
+public class ArtistDao : Dao<T>
 {
     private DBUtil DButil {get; set;}
 
@@ -7,7 +7,7 @@ public class ArtistDao
         DBUtil = new DBUtil();
     }
 
-    public void AddArtist(Artist Artist)
+    public void Add(Artist Artist)
     {
         SqlCommand Cmd = DBUtil.GenerateSql("INSERT INTO Artists(Id, Fname, Lname) VALUES(@Id, @Fname, @Lname)");
         Cmd.Parameters.AddWithValue("@Id", Artist.Id);
@@ -19,9 +19,20 @@ public class ArtistDao
         DBUtil.Disconnect();
     }
   
-    // eg GetArtist("id", "A001")
-    public void GetArtist(string Type, string Value)
+    // eg GetArtist("Id", "A001")
+    public Artist Get(string Type, string Value)
     {
+        SqlCommand Cmd;
         
+        if (Type.Equals("Id"))
+        {
+            Cmd = DBUtil.GenerateSql("SELECT * FROM Artists WHERE ([Id] = @Id)");
+            Cmd.Parameters.AddWtihValue("@Id", Value);
+            
+            SqlDataReader Dr = Cmd.ExecuteReader();
+            
+            // TBC TBC TBC TBC TBC
+            
+        }
     }
 }
