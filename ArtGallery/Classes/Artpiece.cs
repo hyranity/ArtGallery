83cd2e5f2e325ac11ca92e7ba4718577f3a70f40
Ryan <42234515+ollefrost@users.noreply.github.com>
2019-06-15 13:11:32 +0800

    Create Artpiece.cs

---
+++ ArtGallery/Classes/Artpiece.cs
@@ -0,0 +1,24 @@
+public class Artpiece
+{
+  // variables
+  
+  public string ArtistId {get; private set;}
+  public string Title {get; private set;}
+  public string Description {get; private set;}
+  public string ImageUrl {get; private set;}
+  
+  // constructors
+  
+  public Artpiece()
+  {
+  
+  }
+  
+  public Artpiece(string ArtistId, string Title, string Description, string ImageUrl)
+  {
+    this.ArtistId = ArtistId;
+    this.Title = Title;
+    this.Description = Description;
+    this.ImageUrl = ImageUrl;
+  }
+}
